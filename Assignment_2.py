0580c80b31d9c6a2a83784b7c635d36dcd58c457
Nikhil-5654 <59393250+Nikhil-5654@users.noreply.github.com>
2025-07-23 22:57:14 -0400

    Add files via upload

---
+++ Assignment_2.py
@@ -0,0 +1,15 @@
+"""# Task 1
+
+Number=int(input("Enter Your Number: "))
+if Number%2 == 0:
+    print(Number, "is an Odd Number")
+else:
+    print(Number, "is an Even Number")"""
+
+# Task 2
+
+sum = 0
+
+for i in range(51):
+    sum= sum+i
+print('The sum of Numbers from 1 to 50 is: ', sum)
